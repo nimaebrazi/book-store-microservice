2e26efabf5be4dea57fe61a75d8f7d42065b4e62
Nima Ebrazi <nima.ebrazi1373@gmail.com>
2023-05-17 21:10:58 +0330

    ADD: eureka server.

---
+++ eureka-server/src/test/java/com/microservice/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,8 @@
+package com.microservice.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+}
